6837486df5e96acfed65f8d6f9b0fdb3a16181db
David Omenyo <davidomenyo@gmail.com>
2018-11-15 17:53:45 +0000

    deal with extra files

---
+++ Assets/Scripts/DragScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DragScript : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    //private Vector3 offset;
+    private Vector3 originalPosition;
+    public MailboxScript inbox;
+    public MailboxScript sent;
+    public MailboxScript trash;
+    private MailboxScript hoveringOn = null;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //offset = new Vector3(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y);
+        //originalPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (hoveringOn != null)
+        {
+            hoveringOn.addEmail(this.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //transform.position = new Vector3(Input.mousePosition.x - offset.x, Input.mousePosition.y - offset.y, 0);
+        transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        //offset = new Vector3(Input.mousePosition.x - transform.position.x, Input.mousePosition.y - transform.position.y);
+        originalPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        transform.position = originalPosition;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals(trash.name))
+        {
+            hoveringOn = trash;
+            trash.gameObject.GetComponent<Image>().color = new Color32(233, 0, 85, 71);
+        }
+        else if (collision.gameObject.name.Equals(inbox.name))
+        {
+            hoveringOn = inbox;
+            inbox.gameObject.GetComponent<Image>().color = new Color32(233, 0, 85, 71);
+        }
+        else if (collision.gameObject.name.Equals(sent.name))
+        {
+            hoveringOn = sent;
+            sent.gameObject.GetComponent<Image>().color = new Color32(233, 0, 85, 71);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (hoveringOn != null)
+        {
+            hoveringOn.gameObject.GetComponent<Image>().color = new Color32(233, 0, 85, 0);
+        }
+        hoveringOn = null;
+    }
+}
---
+++ Assets/Scripts/MailboxScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MailboxScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public HashSet<GameObject> emails;
+    RaycastHit hitInfo;
+
+    public void Start()
+    {
+        emails = new HashSet<GameObject>();
+    }
+
+    //public HashSet<GameObject> getEmails() {
+    //    return emails;
+    //}
+
+    public void addEmail(GameObject email)
+    {
+        emails.Add(email);
+        printEmails();
+    }
+
+    public void printEmails()
+    {
+        Debug.Log("Emails:");
+        foreach (GameObject email in emails)
+        {
+            Debug.Log(email.name);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color32(139, 139, 139, 71);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color32(139, 139, 139, 0);
+    }
+}
