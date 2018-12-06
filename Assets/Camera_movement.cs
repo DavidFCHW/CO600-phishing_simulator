4a5a672924866b41b70339a8abe8aedb01301cd0
Touniouk <luc.bhayward@gmail.com>
2018-12-06 05:25:55 +0000

    Manager dialogue

--- Assets/Camera_movement.cs
+++ Assets/Camera_movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Camera_movement : MonoBehaviour
 {
+    public bool blocked = true;
 
     public enum RotationAxis
     {
@@ -26,21 +27,24 @@ public class Camera_movement : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        if (axes == RotationAxis.MouseX)
+        if (!blocked)
         {
-            transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
-            _rotationY -= Input.GetAxis("Mouse X") * sensHorizontal;
-            _rotationX = Mathf.Clamp(_rotationX, minimumHori, maximumHori);
-        }
-        else if (axes == RotationAxis.MouseY)
-        {
-            _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
-            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); //Clamps the vertical angle within the min and max variables.
+            if (axes == RotationAxis.MouseX)
+            {
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
+                _rotationY -= Input.GetAxis("Mouse X") * sensHorizontal;
+                _rotationX = Mathf.Clamp(_rotationX, minimumHori, maximumHori);
+            }
+            else if (axes == RotationAxis.MouseY)
+            {
+                _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
+                _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); //Clamps the vertical angle within the min and max variables.
 
 
-            float rotationY = transform.localEulerAngles.y;
+                float rotationY = transform.localEulerAngles.y;
 
-            transform.localEulerAngles = new Vector3(_rotationX, rotationY);
+                transform.localEulerAngles = new Vector3(_rotationX, rotationY);
+            }
         }
     }
 }
---
+++ Assets/Scripts/NextButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextButtonScript : MonoBehaviour {
+
+    public GameObject panel;
+    public Camera_movement camera_Movement;
+
+    public void OnClick()
+    {
+        camera_Movement.blocked = false;
+        Destroy(panel);
+    }
+}
