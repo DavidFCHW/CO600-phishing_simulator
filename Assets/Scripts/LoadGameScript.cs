0b2d1c4bb88aa33e21a348c57c71f88c1f5582a8
David Omenyo <davidomenyo@gmail.com>
2018-12-06 03:59:01 +0000

    functional welcome screen

---
+++ Assets/Scripts/LoadGameScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WelcomeScreenScript : MonoBehaviour {
+
+	public void onClick(){
+        Debug.Log("Loading next scene");
+        SceneManager.LoadScene("Office");
+    }
+}
