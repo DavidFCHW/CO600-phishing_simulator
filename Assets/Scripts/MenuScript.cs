bebca4a0099370e37590534f18ff37a7a89752a0
David Omenyo <davidomenyo@gmail.com>
2019-01-01 17:48:40 +0000

    functional title screen

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript : MonoBehaviour {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Office");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("The game has been exited.");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PopOffScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopOffScript : MonoBehaviour {
+    private Vector3 interogationMarkNormalScale = new Vector3(1, 1, 1);
+    private Vector3 dialogueBoxNormalScale = new Vector3(0, 0, 0);
+
+    public GameObject dialogueBox;
+    public GameObject interogationMark;
+    private bool dialogueBoxShowing = false;
+
+    /*void Awake()
+    {
+        dialogueBox.transform.localScale = new Vector3(0, 0, 0);
+        interogationMark.transform.localScale = new Vector3(0, 0, 0);
+    }*/
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnMouseDown();
+	}
+
+    private void OnMouseDown()
+    {
+        if (dialogueBoxShowing)
+        {
+            dialogueBox.transform.localScale = new Vector3(0, 0, 0);
+            dialogueBoxShowing = false;
+        }
+        else
+        {
+            dialogueBox.transform.localScale = dialogueBoxNormalScale;
+            dialogueBoxShowing = true;
+        }
+    }
+}
