9fc30261ace54d1ed58cec5fbb421d96d9892026
David Omenyo <davidomenyo@gmail.com>
2019-01-02 16:52:21 +0000

    solved 'missing script' issue

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MenuScript : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour {
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene("Office");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
-        Debug.Log("The game has been exited.");
+        Debug.Log("Quit game.");
         Application.Quit();
     }
 }
