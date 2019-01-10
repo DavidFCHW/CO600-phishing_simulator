0efacff31a172ec51413c9650c58410e472ea31c
Luc <touniouk@yahoo.com>
2019-01-10 02:20:58 +0100

    show phishing explanations

--- Assets/Scripts/EmailClient/LinkScript.cs
+++ Assets/Scripts/EmailClient/LinkScript.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class LinkScript : MonoBehaviour {
 
     private TextMeshProUGUI textMeshProText;
-    private int currentLinkActive;
+    private int currentLinkIndexActive;
     private GameObject associatedGameObject;
     public GameObject phishingDefinition;
 
@@ -14,31 +14,37 @@ public class LinkScript : MonoBehaviour {
     {
         textMeshProText = this.GetComponent<TextMeshProUGUI>();
         phishingDefinition.SetActive(false);
-        currentLinkActive = -1;
+        currentLinkIndexActive = -1;
         associatedGameObject = null;
     }
 
     private void LateUpdate()
     {
-        //// Find the link hovered on if any
-        //int hoveredLink = TMP_TextUtilities.FindIntersectingLink(textMeshProText, Input.mousePosition, null);
-        //// Check if it exists and if it's different from the previous one
-        //if (hoveredLink != -1 && hoveredLink != currentLinkActive)
-        //{
-
-        //    // Unselect previously hovered on link
-        //    if (associatedGameObject != null) associatedGameObject.SetActive(false);
-        //    // Assign currentLinkActive
-        //    currentLinkActive = hoveredLink;
-        //    // Check if we're hovering over the word "Phishing"
-        //    if (hoveredLink == 1)
-        //    {
-        //        associatedGameObject = phishingDefinition;
-        //        // Position the panel above the cursor
-        //        associatedGameObject.transform.localPosition = new Vector3(Input.mousePosition.x + 2, Input.mousePosition.y + 2, 0);
-        //        // Make it visible
-        //        associatedGameObject.SetActive(true);
-        //    }
-        //}
+        // Find the link hovered on if any
+        int hoveredLinkIndex = TMP_TextUtilities.FindIntersectingLink(textMeshProText, Input.mousePosition, null);
+        // Check if it exists and if it's different from the previous one
+        if (hoveredLinkIndex != -1 && hoveredLinkIndex != currentLinkIndexActive)
+        {
+            // Get link info based on index
+            TMP_LinkInfo linkInfo = textMeshProText.textInfo.linkInfo[hoveredLinkIndex];
+            // Unselect previously hovered on link
+            if (associatedGameObject != null) associatedGameObject.SetActive(false);
+            // Assign currentLinkActive
+            currentLinkIndexActive = hoveredLinkIndex;
+            // Check if we're hovering over the word "Phishing"
+            if (linkInfo.GetLinkID() == "Phishing definition")
+            {
+                associatedGameObject = phishingDefinition;
+                // Position the panel above the cursor
+                //associatedGameObject.transform.localPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+                // Make it visible
+                associatedGameObject.SetActive(true);
+            }
+        }
+        else if (hoveredLinkIndex == -1 && currentLinkIndexActive != -1)
+        {
+            associatedGameObject.SetActive(false);
+            currentLinkIndexActive = -1;
+        }
     }
 }
