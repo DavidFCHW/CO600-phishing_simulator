b72e347f4d35020c33ab11b7be3305e99ecc1ea9
Luc <touniouk@yahoo.com>
2019-02-04 06:25:25 +0000

    Added sound + fixed garb code for talk scripts

---
+++ Assets/Scripts/OfficeScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Script to coordinate the events in the office
+ */
+public class OfficeScript : MonoBehaviour
+{
+    // Unity object references
+    public GameObject manaherDialogue1;
+    public AudioSource backgroundSound;
+    // Variables
+    private bool managerExplanationsShown = false;
+
+    private void Awake()
+    {
+        // Set every bubble inactive
+        manaherDialogue1.SetActive(false);
+        // Play background sound
+        backgroundSound.Play();
+        // Show manager explanation the first time we enter the office
+        if (!managerExplanationsShown) ShowManagerExplanations();
+        managerExplanationsShown = true;
+    }
+
+    private void ShowManagerExplanations()
+    {
+        manaherDialogue1.SetActive(true);
+    }
+}
